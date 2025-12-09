5a70eaa4fe4e1b807c088dd64800685406ef8927
Jean Sacren <sakiwit@gmail.com>
2025-12-09 04:05:08 -0700

    Add PostSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,9 @@ class DatabaseSeeder extends Seeder
                 'email' => 'test@example.com',
             ]);
         }
+
+        $this->call([
+            PostSeeder::class,
+        ]);
     }
 }
