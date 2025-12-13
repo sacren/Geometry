cb1b0d806396de75a71b69003c67263742d0cc90
Jean Sacren <sakiwit@gmail.com>
2025-12-12 22:51:39 -0700

    Build index.ts out of index.d.ts

---
+++ resources/js/types/index.ts
@@ -0,0 +1,62 @@
+import { InertiaLinkProps } from '@inertiajs/vue3';
+import type { LucideIcon } from 'lucide-vue-next';
+
+export interface Auth {
+    user: User;
+}
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavItem {
+    title: string;
+    href: NonNullable<InertiaLinkProps['href']>;
+    icon?: LucideIcon;
+    isActive?: boolean;
+}
+
+export type AppPageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+    sidebarOpen: boolean;
+};
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    avatar?: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface Post {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    user: User;
+}
+
+export interface PaginationLink {
+    url: string | null;
+    label: string;
+    active: boolean;
+}
+
+export interface PaginatedPosts {
+    data: Post[];
+    links: PaginationLink[];
+}
+
+export interface PostsType {
+    posts: PaginatedPosts;
+}
+
+export type BreadcrumbItemType = BreadcrumbItem;
