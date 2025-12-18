3db19b7e20bdf6cda18c8afa898976b49968e3d2
Jean Sacren <sakiwit@gmail.com>
2025-12-18 03:22:10 -0700

    Update sidebar links
    Use the Github repo of this app in lieu of the vue-starter-kit.  While
    there, remove the Documentation link as it is not applicable any more.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -14,7 +14,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, FileText } from 'lucide-vue-next';
+import { Folder, LayoutGrid, FileText } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 import postsRoute from '@/routes/posts';
 
@@ -34,13 +34,8 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/sacren/Geometry',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
     },
 ];
 </script>
