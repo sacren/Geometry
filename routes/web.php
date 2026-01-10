549589bfea747b89dc974d094e326e0cf6d9993a
Jean Sacren <sakiwit@gmail.com>
2026-01-09 22:58:47 -0700

    Build delete route for posts
    When we use Route::resource() with 'destroy' in the only list, Laravel
    automatically registers this route:
    
    DELETE /posts/{post} → PostController@destroy
    
    And it automatically names it posts.destroy.
    
    Next, we will make sure our PostController::destroy() method includes
    authorization via policy.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('posts', PostController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'destroy'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/settings.php';
