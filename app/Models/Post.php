3f0815998a450a847c753b80e33128373034c444
Jean Sacren <sakiwit@gmail.com>
2026-01-14 23:53:59 -0700

    Add likes() relationship in Post model
    Add required use statement, e.g., use App\Models\Like, by importing the
    Like model since we use Like::class in likes() method.
    
    Additionally, both use App\Models\Like; and use App\Models\User; help
    eliminate ambiguity because explicit is better than implicit.
    
    Use Auth::id() for clarity.  This avoids calling exists() when user is
    guest (safer) and makes the type clearer to static analyzers.  The use
    of Auth class is preferred over that of global auth helper.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Like;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Post extends Model
 {
@@ -29,5 +33,29 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the likes for the post.
+     *
+     * @return HasMany<Like>
+     */
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    /**
+     * Determine if the post has been liked by the current user.
+     */
+    public function likedByCurrentUser(): bool
+    {
+        $userId = Auth::id();
+
+        if ($userId === null) {
+            return false;
+        }
+
+        return $this->likes()->where('user_id', $userId)->exists();
     }
 }
