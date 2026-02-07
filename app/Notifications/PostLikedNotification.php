9f8391d9309bd21dd59e87a77d78fe3024e33fe3
Jean Sacren <sakiwit@gmail.com>
2026-02-06 23:54:16 -0700

    Build Post likes notification

--- app/Notifications/PostLikedNotification.php
+++ app/Notifications/PostLikedNotification.php
@@ -2,8 +2,9 @@
 
 namespace App\Notifications;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -11,18 +12,13 @@ class PostLikedNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public Post $post,
+        public User $liker
+    ) {}
 
     /**
      * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
      */
     public function via(object $notifiable): array
     {
@@ -35,20 +31,23 @@ class PostLikedNotification extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
+            ->subject("Your post was liked!")
+            ->greeting("Hello {$notifiable->name},")
+            ->line("{$this->liker->name} liked your post: '{$this->post->content}'")
+            ->action('View Post', url("/posts/{$this->post->id}"))
             ->line('Thank you for using our application!');
     }
 
     /**
      * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
      */
-    public function toArray(object $notifiable): array
+    public function toArray(object $_notifiable): array
     {
         return [
-            //
+            'post_id' => $this->post->id,
+            'liker_id' => $this->liker->id,
+            'liker_name' => $this->liker->name,
+            'post_content' => $this->post->content,
         ];
     }
 }
